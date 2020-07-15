635afd399b61bd7f760ae173b09bd6286283e64b
Ravana <47528708+ravana69@users.noreply.github.com>
2020-07-15 14:38:38 +0530

    Update config.py

--- userge/config.py
+++ userge/config.py
@@ -70,7 +70,7 @@ class Config:
     GOOGLE_CHROME_DRIVER = os.environ.get("GOOGLE_CHROME_DRIVER", None)
     GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
     LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", 0))
-    UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "https://github.com/UsergeTeam/Userge")
+    UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "https://github.com/ravana69/Userge")
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
     LOAD_UNOFFICIAL_PLUGINS = os.environ.get("LOAD_UNOFFICIAL_PLUGINS", "").lower() == "true"

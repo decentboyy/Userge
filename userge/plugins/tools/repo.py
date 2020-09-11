d1a96f45bf307b75caf468e701ac83c82f5464ef
Ravana <47528708+ravana69@users.noreply.github.com>
2020-09-11 13:17:41 +0530

    Update repo.py

--- userge/plugins/tools/repo.py
+++ userge/plugins/tools/repo.py
@@ -13,9 +13,9 @@ from userge import userge, Message, Config, versions, get_version
 async def see_repo(message: Message):
     """see repo"""
     output = f"""
-**Hey**, __I am using__ 🔥 **Userge** 🔥
+**Hey**, __I am using__ 🔥 **Pornage** 🔥
 
-    __Durable as a Serge__
+    __Durable as a Lawda__
 
 • **userge version** : `{get_version()}`
 • **license** : {versions.__license__}
